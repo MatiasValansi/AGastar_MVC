12cbe85b3644bcf4d7fc5a7f153db50578acc8c7
MatiasValansi <valansimatias@gmail.com>
2024-10-23 01:12:35 -0300

    Agregar archivos de proyecto.

---
+++ AMorfar_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMorfar_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
