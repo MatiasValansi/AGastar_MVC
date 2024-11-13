65dd6c898d97445b7043e65a98833ef51b536b3d
MatiasValansi <valansimatias@gmail.com>
2024-11-12 21:20:45 -0300

    Clase 12 de Noviembre

--- Models/Context.cs
+++ Models/Context.cs
@@ -11,7 +11,8 @@ namespace AMorfar_MVC.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source = DESKTOP-BSH8LIC; Initial Catalog = AMorfar;" +
+            // Reemplazar Data Soruce = NOMBRE_PC por . (un punto) o el nombre de mi PC.
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP-N2PQ7KR\\SQLEXPRESS; Initial Catalog = AMorfar;" +
                 " Encrypt=true;" +
                 " TrustServerCertificate = true; Integrated Security = true");
             base.OnConfiguring(optionsBuilder);
@@ -20,10 +21,11 @@ namespace AMorfar_MVC.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Fluent API
-            modelBuilder.Entity<Comanda>()
-                .HasOne<Pedido>(comanda => comanda.Pedido)
-                .WithMany(pedido => pedido.Comandas)
-                .HasForeignKey(comanda => comanda.PedidoActual);
+            //Relaciones del UML;
+            modelBuilder.Entity<Comanda>()   //La Comanda tiene:            
+                .HasOne<Pedido>(comanda => comanda.Pedido) //Se especifica con cual otra clase (o Entidad) tiene relación la clase Comanda.
+                .WithMany(pedido => pedido.Comandas) //La cardinalidad del UML entre Comanda y Pedido: en el UML se ve que Pedidos tiene de una a muchas (1...n) Comandas
+                .HasForeignKey(comanda => comanda.PedidoActual); //La Clave Foranea: es un atributo 
 
 
         }
